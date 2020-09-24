7b42454897860c89575fa3ef63abeb1899212c38
paigelindahl <paigemarlindahl@gmail.com>
2020-09-24 00:41:07 +0000

    functions that search for words

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) return false;
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (let l of horizontalJoin) {
+      if (l.includes(word)) return true;
     }
-}
-
-module.exports = wordSearch
+    const lettersTransposed = transpose(letters);
+    const verticalJoin = lettersTransposed.map(ls => ls.join(''));
+    for (let l of verticalJoin) {
+      if (l.includes(word)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  };
+  
+  const transpose = function(matrix) {
+    const output = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+      output.push([]);
+    }
+    
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j ++) {
+        output[j].push(matrix[i][j]);
+      }
+    }
+    return output;
+  };
+  
+  module.exports = wordSearch;
